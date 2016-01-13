706620331d30aab4837066abfb4b6da5e6f00028
shabbnam <shabbnam@missionrnd.com>
2016-01-14 01:11:48 +0530

    Submit Commit

--- src/Consonants_Vowels.cpp
+++ src/Consonants_Vowels.cpp
@@ -21,6 +21,29 @@ NOTES: Don't create new string , Dont return anything ,you have been given two p
 
 
 void count_vowels_and_consonants(char *str,int *consonants, int *vowels){
+	*vowels = 0; *consonants = 0;
+	if (str == NULL){ *consonants = 0; *vowels = 0; }
+	else
+	{
+		int i;
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+			{
+				*vowels += 1;
+			}
+			else
+			{
+				if (str[i] != ' '&& !(int(str[i]) < 65))
+				{
+					*consonants += 1;
+				}
+			}
+		}
+
+
+
+	}
 
 
 }
--- src/getLastWord.cpp
+++ src/getLastWord.cpp
@@ -12,5 +12,6 @@ Note:Dont modify original string Neglect Spaces at the right end and at left end
 
 char * get_last_word(char * str){
 
+
 	return NULL;
 }
